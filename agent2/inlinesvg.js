6d826e4be7eef78656afed28b88111e82c13b9e3
Mizuho <53755347+Mizuho-AOKI@users.noreply.github.com>
2021-08-30 22:14:23 +0900

    add function to change color and icon

--- agent2/inlinesvg.js
+++ agent2/inlinesvg.js
@@ -3,12 +3,12 @@
  * Ref : https://www.it-mure.jp.net/ja/css/%E5%A1%97%E3%82%8A%E3%81%A4%E3%81%B6%E3%81%97%E3%81%AE%E8%89%B2%E3%82%92%E5%A4%89%E6%9B%B4%E3%81%99%E3%82%8Bimg-src-svg/1048655414/
  */
 
-function inlineSvg(svgquery){
+function inlineSvg(svgquery, fillcolor, imgsrc){
 jQuery(svgquery).each(function(){
     var $img = jQuery(this);
     var imgID = $img.attr('id');
     var imgClass = $img.attr('class');
-    var imgURL = $img.attr('src');
+    var imgURL = imgsrc ;//? imgsrc : $img.attr('src');
 
     jQuery.get(imgURL, function(data) {
         // Get the SVG tag, ignore the rest
@@ -25,12 +25,18 @@ jQuery(svgquery).each(function(){
         // Remove any invalid XML tags as per http://validator.w3.org
         $svg = $svg.removeAttr('xmlns:a');
 
+        // Layout settings
+        $svg = $svg.attr('style', 'width: 100%; height: 100%; opacity: 1;');
+        $svg.find('path').removeAttr('style');
+        $svg.find(".st0").css({'fill': fillcolor});
+
         // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
         if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
             $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
         }
         // Replace image with new SVG
         $img.replaceWith($svg);
+
     }, 'xml');
 });
 }
